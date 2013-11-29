1222aa5eeca26857c29a81f8ad3eefff94cd4dd7
Matt ODonnell <matt.odonnell@leandog.com>
2013-11-29 14:37:40 -0500

    new replace files

--- replace.py
+++ replace.py
@@ -1,21 +1,22 @@
-from os import listdir
+from os import listdir, walk
 import fnmatch
 from jinja2 import Template
-from optparse import OptionParser
 import json
+import argparse
 
-parser = OptionParser()
-parser.add_option('-j', dest="template_data")
+parser = argparse.ArgumentParser()
+parser.add_argument("template_data")
 
-(options, args) = parser.parse_args()
+args = parser.parse_args()
 
 template_data = json.loads(args.template_data)
 
-for file in listdir("."):
-    if fnmatch.fnmatch("{{app_name}}"):
-        print file
-        #os.rename(file, template_data['app_name"])
+os.renames("{{app_name}}", template_data['app_name'])
 
-
+for (dirpath, dirnames, filenames) in walk('.', topdown=False):
+    print '---------'
+    print dirpath
+    print dirnames
+    print filenames
 
 
