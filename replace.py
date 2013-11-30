8b7411ad3aef4fee50f84e276443cfee349beb3d
Matt ODonnell <matt.odonnell@leandog.com>
2013-11-30 14:10:02 -0500

    test

--- replace.py
+++ replace.py
@@ -6,17 +6,24 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("template_data")
+parser.add_argument("folder")
 
 args = parser.parse_args()
 
 template_data = json.loads(args.template_data)
 
-for (dirpath, dirnames, filenames) in os.walk('.', topdown=False):
+for (dirpath, dirnames, filenames) in os.walk('./' + args.folder, topdown=False):
     for dirname in dirnames:
+        print "doing thing in dir " + dirname
         if fnmatch.fnmatch(dirname, "{{app_name}}"):
             os.rename(dirpath+"/"+dirname, dirpath+"/"+template_data['app_name'])
 
+        print "should be running though files"
+        print filenames
+
         for filename in filenames:
+            print "running " + filename + " though jinja"
+            print "opening " + dirpath + "/" + filename
             fileobj = open(dirpath + "/" + filename, 'r')
             file_contents = fileobj.read()
             fileobj.close()
