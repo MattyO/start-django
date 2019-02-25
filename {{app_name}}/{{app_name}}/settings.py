9a570b789e1c4bfbf758da438aeff9f9225dff4e
matty <odonnell004@gmail.com>
2019-02-24 23:42:45 -0500

    add json import

--- {{app_name}}/{{app_name}}/settings.py
+++ {{app_name}}/{{app_name}}/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import json
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 CONFIG_DIR = os.path.join(BASE_DIR, "config.json")
 
